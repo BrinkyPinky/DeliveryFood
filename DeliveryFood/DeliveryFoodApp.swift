b88fa688749844bb1dbd7cbcc3614e29094fa2d5
BrinyPiny <brinypiny@BrinyPinys-MacBook-Air.local>
2024-01-18 13:24:00 +0500

    initial commit

---
+++ DeliveryFood/DeliveryFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryFoodApp.swift
+//  DeliveryFood
+//
+//  Created by BrinyPiny on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ DeliveryFood/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DeliveryFood
+//
+//  Created by BrinyPiny on 18.01.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("qwr")
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
